153252d034fbf77dde0464eb0da1fd0cd2ab949e
habib.movludov <habib.movludov@aurorabilisim.com>
2021-10-30 21:56:02 +0300

    master

--- src/main/java/com/habibmevlut/simplebankingapp/simplebankingapp/domain/exception/InsufficientBalanceException.java
+++ src/main/java/com/habibmevlut/simplebankingapp/simplebankingapp/domain/exception/InsufficientBalanceException.java
@@ -3,7 +3,8 @@ package com.habibmevlut.simplebankingapp.simplebankingapp.domain.exception;
 public class InsufficientBalanceException extends Exception {
 
     public InsufficientBalanceException() {
-        super("You do not have enough balance for this operation");
+        super();
+        System.err.println("You do not have enough balance for this operation");
     }
 
     public InsufficientBalanceException(String message) {
